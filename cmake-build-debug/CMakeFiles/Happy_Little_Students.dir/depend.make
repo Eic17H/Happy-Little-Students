1b10184efb555ab5ec95fcf744356f93331ab87d
Eic17H <eic17h+github@gmail.com>
2024-05-28 18:23:43 +0200

    Tolti file da gitignorare

--- cmake-build-debug/CMakeFiles/Happy_Little_Students.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Happy_Little_Students.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/Happy_Little_Students.dir/utilita.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Happy_Little_Students.dir/utilita.c.obj: \
- C:\GitHub\Happy-Little-Students\Happy-Little-Students\utilita.c \
- C:\GitHub\Happy-Little-Students\Happy-Little-Students\utilita.h
